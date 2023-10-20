72337cbd45428cf7cf0f52097d831a140e69f541
RetexAOficial <143759569+RetexAOficial@users.noreply.github.com>
2023-10-20 13:28:15 -0300

    JOJOTODINHO

---
+++ quiz.py
@@ -0,0 +1,20 @@
+a = input("Você quer quer eu revele o maior segredo do universo? 1 se quiser e 2 se não quiser ")
+b = input("Você quer iniciar o quis 1 se quiser e 2 se não quiser")
+if a == 1:
+    print("Oque tem dentro dos buracos negros?")
+else:
+    print("okay eu não vou mostrar")
+b = input("Você quer iniciar o quis 1 se quiser e 2 se não quiser")
+if b == "1":
+    c = input("Como a galaxia retangular foi criada? 1 para uma colisão entre galaxias 2 para uma colisão entre planetas e 3 o nosso patio extra indo embora?")
+    d = input("A quanto tempo o big bang aconteceu?  1- 16 bilhoes de anos  2- 13,8 bilhoes de anos  3 - quando a sua avó foi criada?")
+ if c == "1":
+    print("acertor miseseravi")
+else:
+    print("errou!")
+ if d == "2":
+    print("acertor miseseravi")
+else:
+    print("errou!")
+
+
